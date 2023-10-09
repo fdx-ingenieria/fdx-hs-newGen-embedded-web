8050bb2c9ca3bec77ba398373623b2aedbb448ca
German Marquez <gm2404@gmail.com>
2023-10-09 18:22:33 -0300

    feat: add not found page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex absolute w-full h-full bg-red-200 text-center">
+    <div class="lg:px-24 m-auto lg:py-24 md:py-20 md:px-44 px-4 py-24 w-full max-w-screen-xl items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+        <div class="relative">
+          <div class="absolute h-full">
+            <div class="">
+              <h1 class="my-2 text-gray-800 font-bold lg:text-4xl text-2xl">
+                Oops, it seems you've stumbled upon the gateway to the great void.
+              </h1>
+              <p class="my-8">
+                <a href="/" class="sm:w-full lg:w-auto border rounded md py-4 px-8 text-center bg-red-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                  Let's start over!
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="text-gray-50 m-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 10"><text font-family="Consolas" dominant-baseline="middle" text-anchor="middle" font-size="12" font-weight="bold" transform="scale(1.2)" x="42%" y="50%">404</text></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
