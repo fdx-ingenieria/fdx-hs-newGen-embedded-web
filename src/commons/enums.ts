1a1963bfecb33ab712c618b653e3cb090140979e
German Marquez <gm2404@gmail.com>
2023-10-09 18:25:28 -0300

    chore: add commons

---
+++ src/commons/enums.ts
@@ -0,0 +1,39 @@
+export enum SocketStatus {
+  CONNECTING = "Connecting",
+  OPEN = "Open",
+  CLOSING = "Closing",
+  CLOSED = "Closed",
+}
+
+export enum SocketCommands {
+  LABEL = 'label',
+  SENSOR_CONFIG = "sensor_config",
+  ALARM_CONFIG = 'alarm_config',
+  DISCOVERY = 'discovery',
+  NORMAL_MODE = 'normal_mode',
+  MODBUS_CONFIG = 'modbus_config',
+  MODBUS_TABLE = 'modbus_table',
+  NEW_SENSOR_DATA = 'new_sensor_data',
+  ALARM_STATE = 'alarm_states',
+  HS_CONFIG = 'hs_config',
+}
+
+export enum SensorQuality {
+  OUT_OF_SERVICE = "Out of service",
+  BAD = "Bad",
+  REGULAR = "Regular",
+  GOOD = "Good",
+  EXCELLENT = "Excellent",
+}
+
+export enum LabelType {
+  EQUIPMENT = 'equipment',
+  LOCATION = 'location',
+  POSITION = 'position',
+}
+
+export enum ModbusParity {
+  NONE = 'sin paridad',
+  ODD = 'impar',
+  EVEN = 'par',
+}
---
+++ src/commons/interfaces.ts
@@ -0,0 +1,53 @@
+import { LabelType, ModbusParity, SensorQuality } from "./enums";
+
+// Labels
+export interface ILabelData {
+  equipment: string[];
+  location: string[];
+  position: string[];
+}
+
+export interface ILabel {
+  index: number;
+  name: string;
+  type: LabelType;
+}
+
+// Sensors
+export interface ISensorConfig {
+  location: number;
+  position: number;
+  equipment: number;
+}
+
+export interface ISensor {
+  id: number;
+  config: ISensorConfig;
+  quality?: SensorQuality;
+  rssid?: number;
+  time_stamp?: number;
+}
+
+export interface ISensorData {
+  id: number; 
+  avg_temp: number;
+  temp: number;
+  std_dev: number;
+  n_readings: number;
+  quality: SensorQuality;
+  rssid: number;
+  time_stamp: number; // seconds since epoch
+}
+
+// System
+export interface ISystem {
+  serial: number;
+  dir_modbus: number;
+  baudrate: number;
+  bit_paridad: ModbusParity; 
+}
+// Menu & Sidebars
+export interface IMenuItem {
+  label: string;
+  route?: string;
+}
