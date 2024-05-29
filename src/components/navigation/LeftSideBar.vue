e57b892242f5345c629dfd451413ef71ba5885d2
German Marquez <gm2404@gmail.com>
2024-05-29 20:16:58 -0300

    chore: bump version to v0.0.6

--- src/components/navigation/LeftSideBar.vue
+++ src/components/navigation/LeftSideBar.vue
@@ -70,7 +70,7 @@
     </div>
 
     <div class="absolute bottom-0 left-0 justify-left p-4 space-x-4 w-full lg:flex bg-white z-20">
-      <small>Version: 0.0.5</small>
+      <small>Version: 0.0.6</small>
     </div>
   </aside>
 </template>
