9b1fa4ebf4af9f4d2842d2f46b04fe4c150f5d3a
German Marquez <gm2404@gmail.com>
2024-06-07 10:36:24 -0300

    fix(FHNGW-74): improve reactivity

--- src/views/configuration/Sensors.vue
+++ src/views/configuration/Sensors.vue
@@ -11,13 +11,15 @@
   const { getAvailableSensors, getAvailableLabels } = storeToRefs(globalStore)
   const showRightSideBar = ref(false)
   const loadingData = ref(true)
-  const loadingDiscoveryMode = ref(false)
+  const startingDiscoveryMode = ref(false)
+  const stoppingDiscoveryMode = ref(false)
   const savingData = ref(false)
   const editableSensor: Ref<ISensor> = ref({ id: '', EPC: '', config: { equipment: 0, position: 0, location: 0 }})
   const configuredSensors: Ref<ISensor[]> = ref([])
   const unconfiguredSensors: Ref<ISensor[]> = ref([])
   const showUnconfiguredTable = ref(true)
   const showResetButton = ref(false)
+  const MAX_CONFIGURED_SENSORS = 17
 
   watch(getAvailableSensors, (newData) => {
     configuredSensors.value = []
@@ -33,8 +35,12 @@
   })
 
   const isLimitReached = computed(() => {
-    return getAvailableSensors.value.length >= 50;
+    return configuredSensors.value.length >= MAX_CONFIGURED_SENSORS;
   });
+
+  const isUnconfigured = (sensor: ISensor) => {
+    return unconfiguredSensors.value.some((item: ISensor) => item.id === sensor.id)
+  }
 
   const editSensor = (id: string) => {
     let sensor = getAvailableSensors.value.find((item: ISensor) => item.id === id)
@@ -43,13 +49,16 @@
 
     // Deep copy prevents the reactivity of the original object
     editableSensor.value = JSON.parse(JSON.stringify(sensor))
-    showResetButton.value = !!sensor.config.equipment && !!sensor.config.position && !!sensor.config.location
+    showResetButton.value = !isUnconfigured(editableSensor.value)
     showRightSideBar.value = true
   }
 
   const isComplete = (editableSensor: ISensor) => {
     const { equipment, position, location } = editableSensor.config
-    return !!equipment && !!position && !!location && isLimitReached
+
+    if (isUnconfigured(editableSensor) && isLimitReached.value) return false
+
+    return !!equipment && !!position && !!location
   }
 
   const saveSensor = () => {
@@ -80,13 +89,17 @@
     save()
   }
 
-  const switchDiscoveryMode = () => {
-    loadingDiscoveryMode.value = true
-    const promise = globalStore.getDiscoveryModeOn
-      ? globalStore.stopDiscoveryMode().then(() => globalStore.startNormalMode())
-      : globalStore.startDiscoveryMode()
-
-      promise.finally(() => loadingDiscoveryMode.value = false)
+  const stopDiscoveryMode = () => {
+    stoppingDiscoveryMode.value = true
+    globalStore.stopDiscoveryMode()
+      .then(() => globalStore.startNormalMode())
+      .finally(() => stoppingDiscoveryMode.value = false)
+  }
+  const startDiscoveryMode = () => {
+    startingDiscoveryMode.value = true
+    globalStore.stopNormalMode()
+      .then(() => globalStore.startDiscoveryMode())
+      .finally(() => startingDiscoveryMode.value = false)
   }
 
   const clearUnconfiguredSensors = () => {
@@ -162,10 +175,10 @@
           <p v-show="!editableSensor.config.position" class="mt-2 text-sm text-red-600"><span class="font-semibold">Oops!</span> This is required!</p>
         </div>
       </div>
-      <div v-if="isLimitReached" class="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 mt-2" role="alert">
+      <div v-if="isLimitReached && isUnconfigured(editableSensor)" class="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 mt-2" role="alert">
         <AlertIcon class="flex-none w-5 mr-3" />
         <div class="ml-3 text-sm font-medium">
-          Hey, you've reached the limit of 50 sensors.
+          Hey, you've reached the limit of {{ MAX_CONFIGURED_SENSORS }} sensors.
         </div>
       </div>
       <div v-if="checkDuplicate(editableSensor)" class="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 mt-2" role="alert">
@@ -213,7 +226,7 @@
             <button type="button"
               v-if="unconfiguredSensors.length"
               @click="clearUnconfiguredSensors()"
-              :disabled="loadingData || loadingDiscoveryMode"
+              :disabled="loadingData || startingDiscoveryMode"
               class="inline-flex items-center disabled:opacity-50 text-yellow-700 bg-white border border-gray-300 focus:outline-none hover:bg-yellow-700 hover:border-yellow-700 hover:text-white focus:ring-0 font-semibold rounded-md text-sm sm:text-md px-3 py-.5">
               <template v-if="loadingData">
                 <LoadingIcon class="w-4 h-4 animate-spin fill-transparent mx-auto mr-1" />
@@ -225,11 +238,11 @@
               </template>
             </button>
             <button type="button"
-              v-if="!globalStore.getDiscoveryModeOn"
-              @click="switchDiscoveryMode"
-              :disabled="loadingData || loadingDiscoveryMode"
+              v-if="globalStore.getNormaModeOn || startingDiscoveryMode"
+              @click="startDiscoveryMode"
+              :disabled="loadingData || startingDiscoveryMode"
               class="inline-flex items-center disabled:opacity-50 text-green-700 bg-white border border-gray-300 focus:outline-none hover:bg-green-700 hover:border-green-700 hover:text-white focus:ring-0 font-semibold rounded-md text-sm sm:text-md px-3 py-.5">
-              <template v-if="loadingDiscoveryMode">
+              <template v-if="startingDiscoveryMode">
                 <LoadingIcon class="w-4 h-4 animate-spin fill-transparent mx-auto mr-1" />
                 Starting...
               </template>
@@ -239,11 +252,11 @@
               </template>
             </button>
             <button type="button"
-              v-if="globalStore.getDiscoveryModeOn"
-              @click="switchDiscoveryMode"
-              :disabled="loadingData || loadingDiscoveryMode"
+              v-if="globalStore.getDiscoveryModeOn || stoppingDiscoveryMode"
+              @click="stopDiscoveryMode"
+              :disabled="loadingData || stoppingDiscoveryMode"
               class="flex items-center text-red-700 bg-white border border-gray-300 focus:outline-none hover:bg-red-600 hover:border-red-600 hover:text-white focus:ring-0 font-semibold rounded-md text-md px-3 py-.5">
-              <template v-if="loadingDiscoveryMode">
+              <template v-if="stoppingDiscoveryMode">
                 <LoadingIcon class="w-4 h-4 animate-spin fill-transparent mx-auto mr-1" />
                 Stopping...
               </template>
@@ -268,7 +281,7 @@
           <h3 class="text-xl font-semibold">Configured sensors</h3>
         </div>
         <LoadingIcon v-if="loadingData" class="w-8 h-8 animate-spin text-fdx-red fill-transparent mx-auto my-4" />
-        <SensorTable v-else :availableSensors="configuredSensors" :showlabels="true" @edit="editSensor" @reset="resetSensor" :max="50" />
+        <SensorTable v-else :availableSensors="configuredSensors" :showlabels="true" @edit="editSensor" @reset="resetSensor" :max="MAX_CONFIGURED_SENSORS" />
       </div>
     </div>
   </section>
