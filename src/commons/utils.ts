f05bacbdac1ee9ce6c2414f24dcca25eee6d037c
German Marquez <gm2404@gmail.com>
2024-06-11 08:35:00 -0300

    refactor: Implement custom logger to restrict logging in prod

--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -3,3 +3,9 @@ export  const isValidInteger = (value: number): boolean => {
   return Number.isFinite(value)
   && parseInt(value.toString()) === value
 }
+
+export const customLog = (...args: any) => {
+  if (import.meta.env.DEV) {
+    console.log(...args);
+  }
+};
